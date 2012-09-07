e1c70d3a9cbb12d967cc40a257e0d431af3f1680
Charles Ferte <cferte@belltown.fhcrc.org>
2012-09-07 09:08:25 -0700

    added random forest

--- transbig_try.R
+++ transbig_try.R
@@ -1,6 +1,31 @@
+#charles Ferté
+
+
 ###
 require(Biobase)
 require(randomForest)
 require(breastCancerTRANSBIG)
-#
+
+###
+data(transbig)
+transbig@phenoData
+EXP <- exprs(transbig)
+CLIN <- phenoData(transbig)
+
+# separtae the dataset into a TS and a VS
+set.seed(070912)
+randVec <- rbinom(dim(transbig)[2],size=1, prob=.5)
+T_EXP <- EXP[,randVec==0]
+V_EXP <- EXP[,randVec==1]
+
+T_CLIN <- CLIN$er[randVec==0]
+V_CLIN <- CLIN$er[randVec==1]
+
+## model the ER status (binary) using RandomForest
+fit <- randomForest(x=t(T_EXP),y=T_CLIN,ntree=200, do.trace=10)
+
+
+
+
+
 
