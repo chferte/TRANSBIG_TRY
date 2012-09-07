164bacbab32398777d6c77d69fd8e86b4953273d
Charles Ferte <cferte@belltown.fhcrc.org>
2012-09-07 09:22:53 -0700

    add ROCR

--- transbig_try.R
+++ transbig_try.R
@@ -21,9 +21,19 @@ V_EXP <- EXP[,randVec==1]
 T_CLIN <- CLIN$er[randVec==0]
 V_CLIN <- CLIN$er[randVec==1]
 
-## model the ER status (binary) using RandomForest
-fit <- randomForest(x=t(T_EXP),y=T_CLIN,ntree=200, do.trace=10)
+## model the ER status (binary) using RandomForest using T_EXP
+fit <- randomForest(x=t(T_EXP),y=factor(T_CLIN),ntree=200, do.trace=10)
 
+## aapply the fit on the V_EXP
+Validation_Score_Hat <- predict(fit,t(V_EXP),type="prob")
+
+# plot a ROC curve to asses the response
+require(ROCR)
+erPred <- prediction(as.numeric(Validation_Score_Hat[,2]),as.numeric(V_CLIN))
+erPerf <- performance(prediction.obj=erPred,"tpr","fpr")
+erAUC <- performance(prediction.obj=erPred,"auc")
+plot(erPerf, col=rainbow(10))
+text(x=.7,y=.4,labels=paste("AUC=",erAUC@y.values))
 
 
 
